d9877be89377c8cfaea554e7a6861c6bdc0e5d06
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-21 23:43:57 +0100

    mod: change order of arguments

--- src/components/contribute/Capability.js
+++ src/components/contribute/Capability.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Capability = ({ heading, description, iconClass }) =>
+const Capability = ({ iconClass, heading, description }) =>
   <section>
     <span className={`icon major ${iconClass}`} />
     <h3>{heading}</h3>
