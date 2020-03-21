139794a5705e08fd03ae9f1423e26ae4b8b18d2d
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-21 04:46:33 +0100

    fix: hardcoded SEO image

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -28,6 +28,7 @@ const Layout = ({ children }) => (
             { property: 'og:image', itemprop: 'image', content: 'https://www.solidaric.org/static/solidaric-org-social-fix-image-1200x630.png' },
             { property: 'og:image:type' content: 'image/png' },
             { property: 'og:url', content: 'https://www.solidaric.org/de' },
+            { property: 'og:type', content: 'website' },
             { property: 'twitter:card', content: 'summary_large_image' },
             { property: 'twitter:title', content: 'Solidaric.org - Menschen für Menschen' },
             { property: 'twitter:description', content: 'Gemeinsam in Krisenzeiten - gemeinsam im Leben.' },
