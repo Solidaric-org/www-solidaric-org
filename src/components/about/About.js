35c80ad64e52a61f5cb170b28dcddea717173bd2
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-24 12:47:42 +0100

    mod: shorten code by ES6

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,12 +3,9 @@ import React from 'react';
 import FeatureList from '../feature/FeatureList';
 import ABOUT_ITEMS from './AboutItems';
 
-const About = () => {
-  return (
-    <section id="about" className="wrapper style1 spotlights">
-       {FeatureList(ABOUT_ITEMS)}
-    </section>
-  );
-}
+const About = () =>
+  <section id="about" className="wrapper style1 spotlights">
+     {FeatureList(ABOUT_ITEMS)}
+  </section>
 
 export default About;
