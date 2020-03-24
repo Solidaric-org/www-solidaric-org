05740c3532bc36ce1fb98eef6747f324587dd679
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-24 18:06:18 +0100

    mod: re-arrange data and link for title if to.href exists

--- src/components/capability/Capability.js
+++ src/components/capability/Capability.js
@@ -4,18 +4,21 @@ import parseHTML from 'html-react-parser';
 
 const Capability = ({ heading, description, iconClass, to }) =>
   <section>
-    { heading !== '' &&
-      <span className={`icon major ${iconClass}`} />
-    }
-    <h3>{parseHTML(heading)}</h3>
-    <p>{parseHTML(description)}</p>
-    { to &&
-    <div className="bottom-wrapper">
-      { to.href !== '' &&
-        <div className="actions">
-          <a className='button' href={to.href}>{to.label === '' ? heading : to.label}</a>
-        </div>
+    <span className={`icon major ${iconClass}`} />
+    <h3>
+      {to
+        ? <a href={to.href}>{heading}</a>
+        : <>{heading}</>
       }
+    </h3>
+    <p>
+      {parseHTML(description)}
+    </p>
+    { to && to.label &&
+    <div className="bottom-wrapper">
+      <div className="actions">
+        <a className='button' href={to.href}>{to.label}</a>
+      </div>
     </div>
     }
   </section>
