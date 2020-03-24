b7c35e02845955a2d869b23f050001c307a9c876
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-24 11:03:08 +0100

    mod: allow list of capabilities from array or via graphQL results

--- src/components/capability/CapabilityList.js
+++ src/components/capability/CapabilityList.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Capability from './Capability';
 
-const CapabilityList = (capabilities) => {
-  return (
-    <>
-      {capabilities.map((capability) => <Capability key={capability.heading} {...capability} />)}
-    </>
+const CapabilityList = (capabilities) =>
+  capabilities.map((capability) =>
+    <Capability
+      key={capability.node ? capability.node.heading : capability.heading}
+      {... capability.node ? capability.node : capability}
+    />
   )
-}
 
 export default CapabilityList;
