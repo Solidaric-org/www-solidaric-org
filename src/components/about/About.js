2770eedb7bdfbda0eafe44eaf942edf27be00575
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-24 17:03:15 +0100

    mod: reorg code and webpack

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import FeatureList from '../feature/FeatureList';
-import ABOUT_ITEMS from './AboutItems';
+import ABOUTS_DATA from './data/Abouts.yaml'
+
+// include (ALL) the images from about/images to webpack and
+// rewrite reference string from YAML into loaded object
+ABOUTS_DATA.map(feature => {feature.image = require("./images/" + feature.image)})
 
 const About = () =>
   <section id="about" className="wrapper style1 spotlights">
-     {FeatureList(ABOUT_ITEMS)}
+     {FeatureList(ABOUTS_DATA)}
   </section>
 
 export default About;
