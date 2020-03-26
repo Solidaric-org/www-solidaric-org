8b651c73e0ae2a363d28c1bf029117d5a1281334
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-26 12:18:31 +0100

    mod: simplifying page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import Layout   from '../../components/common/layout';
-import Footer   from '../../components/common/Footer';
-import Sidebar  from '../../components/common/Sidebar';
+import Layout   from '../components/common/layout';
+import Footer   from '../components/common/Footer';
+import Sidebar  from '../components/common/Sidebar';
 
-import Start        from '../../components/start/Start';
-import About        from '../../components/about/About';
-import Contribute   from '../../components/contribute/Contribute';
-import Engage       from '../../components/engage/Engage';
-import Contact      from '../../components/contact/Contact';
+import Start        from '../components/start/Start';
+import About        from '../components/about/About';
+import Contribute   from '../components/contribute/Contribute';
+import Engage       from '../components/engage/Engage';
+import Contact      from '../components/contact/Contact';
 
 const IndexPage = () =>
   <Layout>
