63a74b23f094659699bfbc46c165ed1280e94116
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-28 20:56:03 +0100

    mod: updated build for gh-pages

---
+++ docs/app-8202909b983e06c8fcca.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],[],[["UxWs",4,2,0]]]);
+//# sourceMappingURL=app-8202909b983e06c8fcca.js.map
--- docs/styles-48f592945cb2d47ac09e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{kpl1:function(n,p,w){}}]);
-//# sourceMappingURL=styles-48f592945cb2d47ac09e.js.map
