85dfc8b785689816950317e5e8ce5214e3020d5d
Tom Freudenberg <tom.freudenberg@4commerce.de>
2020-03-30 17:48:49 +0200

    add: client side reader for visit tracking

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,20 +1,36 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react"
 import '../../assets/sass/main.scss';
 
-const Footer = () => (
-  <footer id="footer" className="wrapper alt">
-    <div className="inner">
-      <ul className="menu">
-        <li>
-          &copy; 2020 Solidaric.org, all rights reserved.
-        </li>
-        <li>
-          <a href="/#contact">Impressum</a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  // Client-side Runtime Data Fetching
+  const [hitCount, setHitCount] = useState(0);
+  useEffect(() => {
+    let query ='https://hitcounter.pythonanywhere.com/count' + '?url=' + encodeURIComponent('www.solidaric.org')
+    // get data from counter api
+    fetch(query)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setHitCount(resultData)
+      }) // just set data for the number of hits
+  }, [])
+
+  return(
+    <footer id="footer" className="wrapper alt">
+      <div className="inner">
+        <ul className="menu">
+          <li>
+            &copy; 2020 Solidaric.org, all rights reserved.
+          </li>
+          <li>
+            <a href="/#contact">Impressum</a>
+          </li>
+          <li>
+            {hitCount} Besuche
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer;
